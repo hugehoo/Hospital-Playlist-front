b2dba97b40e7d0184c33247a25d49c666f8803d0
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-10-05 16:57:00 +0900

    [mod] cancel lazyloading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,30 +1,25 @@
-import React, {lazy, Suspense} from 'react';
+import React, {Suspense} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import '../style/App.css';
-// import Main from "./Main";
-// import TestPage from "./TestPage"
-// import ResultPage from "./ResultPage";
-// import TempPage from "./TempPage";
-// import ErrorPage from "./ErrorPage";
+import Main from "./Main";
+import TestPage from "./TestPage"
+import ResultPage from "./ResultPage";
+import ErrorPage from "./ErrorPage";
+import TempPage from "./TempPage";
 
-const MainPage = lazy(() => import("./Main"))
-const TestPage = lazy(() => import("./TestPage"))
-const TempPage = lazy(() => import(".//TempPage"))
-const ResultPage = lazy(() => import("./ResultPage"))
-const ErrorPage = lazy(() => import("./ErrorPage"))
 
 function App() {
     return (
         <div className="App">
             <Suspense fallback={<div>Loading...</div>}>
                 <header className="App-header">
-                        <Switch>
-                            <Route exact path="/" component={MainPage}/>
-                            <Route path="/testpage" component={TestPage}/>
-                            <Route path="/temppage" component={TempPage}/>
-                            <Route path="/resultpage" component={ResultPage}/>
-                            <Route component={ErrorPage}/>
-                        </Switch>
+                    <Switch>
+                        <Route path="/" component={Main} exact/>
+                        <Route path="/testpage" component={TestPage}/>
+                        <Route path="/temppage" component={TempPage}/>
+                        <Route path="/resultpage" component={ResultPage}/>
+                        <Route component={ErrorPage}/>
+                    </Switch>
                 </header>
             </Suspense>
         </div>
