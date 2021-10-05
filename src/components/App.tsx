5d5095962c90b57dedaa185363d5813d81decacc
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-10-05 18:04:12 +0900

    [mod] lazyloading 전체 해제

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,17 @@
-import React, {lazy, Suspense} from 'react';
+import React, {Suspense} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import '../style/App.css';
-// import Main from "./Main";
-// import TestPage from "./TestPage";
-// import ResultPage from "./ResultPage";
-// import ErrorPage from "./ErrorPage";
-// import TempPage from "./TempPage";
+import Main from "./Main";
+import TestPage from "./TestPage";
+import ResultPage from "./ResultPage";
+import ErrorPage from "./ErrorPage";
+import TempPage from "./TempPage";
 
-const Main = lazy(() => import("./Main"))
-const TestPage = lazy(() => import("./TestPage"))
-const ResultPage = lazy(() => import("./ResultPage"))
-const ErrorPage = lazy(() => import("./ErrorPage"))
-const TempPage = lazy(() => import("./TempPage"))
+// const Main = lazy(() => import("./Main"))
+// const TestPage = lazy(() => import("./TestPage"))
+// const ResultPage = lazy(() => import("./ResultPage"))
+// const ErrorPage = lazy(() => import("./ErrorPage"))
+// const TempPage = lazy(() => import("./TempPage"))
 
 function App() {
     return (
