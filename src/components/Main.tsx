b774bea84eb823a6a94e0e8148ebd0d1805f6156
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-10-15 10:51:43 +0900

    [mod] 메인이미지 jpeg 확장자로 변경

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,18 @@
 import "../style/Main.css"
-import poster from "../images/슬의.avif"
+import poster from "../images/슬의.jpeg"
 import {Link} from "react-router-dom";
-import {useEffect} from "react";
+import {lazy, useEffect} from "react";
 import {useResetRecoilState} from "recoil";
 import {IsError, IsLoading, QuestionIdx, ResponseData} from "../store/store";
+const lazyWithPreload = (importFunction:any) => {
+    const Component = lazy(importFunction);
+    //@ts-ignore
+    Component.preload = importFunction;
+    // 이게 의미하는게 뭐야. Component 에 원래 preload 라는 property 가 존재하는 거야. 아니면 이 라인에서 주입해주는거야?
+    // 원래 프로퍼티는 있는데, 이 때 정의를 해주는 것 같다. 이전까진 undefined 상태.
+    return Component
+}
+const LazyImageModal = lazyWithPreload(() => import("../images/슬의.jpeg"))
 
 const Main = () => {
     const resetIdx = useResetRecoilState(QuestionIdx)
@@ -17,6 +26,21 @@ const Main = () => {
         error()
         loading()
     }, [resetIdx, responseData, error, loading])
+
+    //@ts-ignore
+    const getParametersForUnsplash = ({width, height, quality, format}) => {
+        return `?w=${width}&h=${height}&q=${quality}&fm=${format}&fit=crop`
+    }
+
+    useEffect(() => {
+        //@ts-ignore
+        LazyImageModal.preload();
+        const img = new Image();
+        img.src = "./images/슬의.jpeg"
+
+    }, []);
+
+
 
     return (
         <section id="main_contents">
@@ -34,7 +58,15 @@ const Main = () => {
                         "position": "relative",
                         "top": "-20px",
                     }}>
-                        <img id="poster" width="400" height="282" loading="lazy" src={poster} alt="슬의"/>
+                        <img id="poster" loading="lazy" src={poster
+                        + getParametersForUnsplash({
+                            width: '380px',
+                            height: '282px',
+                            quality: 80,
+                            format: 'png'
+                        })} alt="슬의"
+                             style={{"width": "380px", "height":"282px"}}
+                        />
                     </div>
                 </div>
                 <div className="bottom">
@@ -51,6 +83,9 @@ const Main = () => {
                         <button className="start-button" type="button">START</button>
                     </Link>
                 </div>
+            </div>
+            <div>
+                <a href="javascript:sendLink()"><img src="https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium.png" /></a>
             </div>
         </section>
     )
