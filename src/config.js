3c34db18e971cdcd906f2b47895715aa41d18bf4
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-10-17 18:40:29 +0900

    [del] 잘못되 config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export const SERVER = "https://hp-back.herokuapp.com/hospital"
-process.env.PUBLIC_URL = "https://friendly-shannon-fbae91.netlify.app"
 // export const SERVER = "http://localhost:5000/hospital"
 
 
