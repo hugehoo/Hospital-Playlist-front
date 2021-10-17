a2dd1b415246787ac1e8116c0a9f4ff4fc7a3bf6
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-10-17 18:38:20 +0900

    [mod] kakao share config

--- src/components/KakaoShareButton.tsx
+++ src/components/KakaoShareButton.tsx
@@ -22,7 +22,8 @@ const KakaoShareButton = () => {
                 content: {
                     title: '슬기로운 의사생활 심리테스트',
                     description: '#시즌3 #기원 #슬의심리테스트',
-                    imageUrl: "../images/슬의.jpeg", // i.e. process.env.FETCH_URL + '/logo.png'
+                    imageUrl: process.env.PUBLIC_URL + "/static/media/%EC%8A%AC%EC%9D%98.194ae169.jpeg?w=380px&h=282px&q=80&fm=png&fit=crop",
+                    // https://friendly-shannon-fbae91.netlify.app/static/media/%/**/EC%8A%AC%EC%9D%98.194ae169.jpeg?w=380px&h=282px&q=80&fm=png&fit=crop
                     link: {
                         mobileWebUrl: window.location.href,
                         webUrl: window.location.href,
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export const SERVER = "https://hp-back.herokuapp.com/hospital"
+process.env.PUBLIC_URL = "https://friendly-shannon-fbae91.netlify.app"
 // export const SERVER = "http://localhost:5000/hospital"
 
 
